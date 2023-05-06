8fce1250f8babd297e8e618d71cb1c3ade4b4004
Angel Daniel (Kaniltazo) <angeldanielrm@icloud.com>
2023-05-05 19:46:29 -0500

    Ajustes

--- apprestaurant/src/components/Menu.js
+++ apprestaurant/src/components/Menu.js
@@ -6,8 +6,9 @@ const Menu = () => {
     return (
         <div className='container'>
             <body>
-            <p>Numero de Personas = </p>{state}
+            <p>Numero de Personas = </p>
             <br/>
+            
             <button className='boton' onClick={() => setState(state + 1)}><span></span>
                 Agregar Persona +1
                 <span></span></button>
